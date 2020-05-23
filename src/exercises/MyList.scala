2eca664a61d5a9c68ea9fb253d932de2ab530931
Yingying Chen <ychen1030@berkeley.edu>
2020-05-23 12:38:55 -0700

    add what's a function

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -9,9 +9,10 @@ abstract class MyList[+A] {
   def printElements: String
   override def toString(): String = "[" + printElements + "]"
 
-  def map[B](transformer: MyTransformer[A, B]): MyList[B]
-  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
-  def filter(predicate: MyPredicate[A]): MyList[A]
+  // higher-order functions
+  def map[B](transformer: A => B): MyList[B]
+  def flatMap[B](transformer: A => MyList[B]): MyList[B]
+  def filter(predicate: A => Boolean): MyList[A]
 
   // concatenation
   def ++[B >: A](list: MyList[B]): MyList[B]
@@ -24,9 +25,9 @@ case object Empty extends MyList[Nothing] {
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 
-  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
-  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
-  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def map[B](transformer: Nothing => B): MyList[B] = Empty
+  def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+  def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
@@ -41,29 +42,29 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     else head + " " + tail.printElements
   }
 
-  def map[B](transformer: MyTransformer[A, B]): MyList[B] = {
-    new Cons(transformer.transform(h), t.map(transformer))
+  def map[B](transformer: A => B): MyList[B] = {
+    new Cons(transformer(h), t.map(transformer))
   }
 
-  def filter(predicate: MyPredicate[A]): MyList[A] = {
-    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+  def filter(predicate: A => Boolean): MyList[A] = {
+    if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
   }
 
   def ++[B >: A](list: MyList[B]): MyList[B] = new Cons[B](h, t ++ list)
 
-  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = {
-    transformer.transform(h) ++ t.flatMap(transformer)
+  def flatMap[B](transformer: A => MyList[B]): MyList[B] = {
+    transformer(h) ++ t.flatMap(transformer)
   }
 }
 
-trait MyPredicate[-T] {
-  def test(elem: T): Boolean
-}
-
-trait MyTransformer[-A, B] {
-  def transform(elem: A): B
-}
+//trait MyPredicate[-T] { // T => Boolean
+//  def test(elem: T): Boolean
+//}
+//
+//trait MyTransformer[-A, B] { // A => B
+//  def transform(elem: A): B
+//}
 
 object ListTest extends App {
   val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
@@ -79,17 +80,17 @@ object ListTest extends App {
   println(listOfIntegers.toString())
   println(listOfStrings.toString())
 
-  println(listOfIntegers.map(new MyTransformer[Int, Int] {
-    override def transform(elem: Int): Int = elem * 2
+  println(listOfIntegers.map(new Function1[Int, Int] {
+    override def apply(elem: Int): Int = elem * 2
   }).toString())
 
-  println(listOfIntegers.filter(new MyPredicate[Int] {
-    override def test(elem: Int): Boolean = elem % 2 == 0
+  println(listOfIntegers.filter(new Function1[Int, Boolean] {
+    override def apply(elem: Int): Boolean = elem % 2 == 0
   }).toString())
 
   println((listOfIntegers ++ anotherListOfIntegers).toString())
-  println(listOfIntegers.flatMap(new MyTransformer[Int, MyList[Int]] {
-    override def transform(elem: Int): MyList[Int] = new Cons[Int](elem, new Cons[Int](elem + 1, Empty))
+  println(listOfIntegers.flatMap(new Function1[Int, MyList[Int]] {
+    override def apply(elem: Int): MyList[Int] = new Cons[Int](elem, new Cons[Int](elem + 1, Empty))
   }))
 
   // case class
---
+++ src/lectures/part3fp/WhatsAFunction.scala
@@ -0,0 +1,55 @@
+package lectures.part3fp
+
+object WhatsAFunction extends App {
+
+  // DREAM: use functions as first class elements
+  // problem: oop
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int = element * 2
+  }
+
+  println(doubler(2))
+
+  // function types = Function1[A, B]
+  val stringToIntConverter = new Function1[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+  println(stringToIntConverter("3") + 4)
+
+  val adder: ((Int, Int) => Int) = new Function2[Int, Int, Int] {
+    override def apply(v1: Int, v2: Int): Int = v1 + v2
+  }
+
+  // Function types Function2[A, B, R] === (A, B) => R
+
+  // ALL scala functions are objects
+
+  /*
+    1. a function which takes 2 strings and concatenates them
+    2. transform the MyPredicate and MyTransformer into function types
+    3. define a function which takes an int and returns another function,
+       which takes an int and returns an int
+       - what's the type of this function
+       - how to do it
+   */
+
+  def concatenator: (String, String) => String = {
+    (v1: String, v2: String) => v1 + v2
+  }
+  println(concatenator("Hello ", "Scala!"))
+
+  // Function1[Int, Function1[Int, Int]]
+  val superAdder: Function1[Int, Function1[Int, Int]] = new Function1[Int, Function1[Int, Int]] {
+    override def apply(x: Int): Int => Int = new Function1[Int, Int] {
+      override def apply(y: Int): Int = x + y
+    }
+  }
+  val adder3 = superAdder(3)
+  println(adder3(4))
+  println(superAdder(3)(4)) // curried function
+}
+
+trait MyFunction[A, B] {
+  def apply(element: A): B
+}
